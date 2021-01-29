51c184c94a66e7f20608bacb7a0e09c7f11a6c94
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-01-29 22:23:45 +0530

    styling and form

--- index.js
+++ index.js
@@ -11,9 +11,44 @@ app.set('view engine','ejs');
 
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded())
+
+app.use(express.static('assets'));
+
+// //middleware1
+// app.use(function(req,res,next){
+//     console.log('middleware 1 called');
+//     next();
+// });
+
+// //middleware2
+// app.use(function(req,res,next){
+//     console.log('middleware 2 called');
+//     next();
+// });
+
+var contactsList = [
+    {
+        name:"Tanmay",
+        phone:'1111111111'
+    },
+    {
+        name:"Tejas",
+        phone:'2222222222'
+    },
+    {
+        name:"John",
+        phone:'4444444444'
+    }
+]
+
+
 app.get('/', function(req,res){
 
-    return res.render('home', {title:'Contact List'} );
+    return res.render('home', {
+        title:'Contact List',
+        contact_list: contactsList
+    });
 
 });
 
@@ -23,8 +58,22 @@ app.get('/prac', function(req,res){
         title:'Contact List'
     } );
 
-})
+});
 
+
+app.post('/create-contact', function(req,res){
+    // return res.redirect('/prac');
+    // console.log(req.body);
+    // console.log(req.body.name);
+    // console.log(req.body.phone);
+    // contactsList.push({
+    //         name:req.body.name,
+    //         phone:req.body.phone
+    //     });
+    // return res.redirect('/');
+    contactsList.push(req.body);
+    return res.redirect('back');
+});
 
 
 
